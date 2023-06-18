d5ed8173fcb4a85a41a421362605724e37326927
Pavi-thra-27 <paviyaramu27@gmail.com>
2023-06-18 03:39:33 -0700

    welcome page changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Unibio-Website';
+
+  ngOnInit() {
+    const showToast = document.getElementById('#welcomeToast')!;
+    console.log(showToast);
+    // showToast.toast('show');
+  }
 }
