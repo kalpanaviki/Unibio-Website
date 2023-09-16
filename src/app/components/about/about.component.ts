b746c866c49ed8feaf8bc0b3ce372d8226575c5d
kalpanaviki <68535954+kalpanaviki@users.noreply.github.com>
2023-09-16 14:38:08 +0530

    Halfway-Happy

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Properties } from '../../appConstants/aboutConstants';
 
 @Component({
@@ -12,13 +12,33 @@ export class AboutComponent implements OnInit {
   array3: any = [];
   array4: any = [];
 
+  isLaptop = false;
+  isMobile = false;
+
+  constructor() {}
+
   init() {
     this.array1 = Properties.ABOUT_FIRST_ARRAY;
     this.array2 = Properties.ABOUT_SECOND_ARRAY;
     this.array3 = Properties.ABOUT_THIRD_ARRAY;
     this.array4 = Properties.ABOUT_FOURTH_ARRAY;
   }
+
   ngOnInit() {
     this.init();
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize(): void {
+    const screenWidth = window.innerWidth;
+    const breakpoint = 480; 
+
+    this.isLaptop = screenWidth > breakpoint;
+    this.isMobile = screenWidth <= breakpoint;
   }
 }
