240279e34c55ed42599fe2ff023600e48f6ad43a
kalpanaviki <68535954+kalpanaviki@users.noreply.github.com>
2023-11-08 16:31:37 +0530

    Operation Smiley

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,10 +1,16 @@
-import { Component} from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent{
+export class MainComponent implements AfterViewInit {
+  @ViewChild('videoRef', { static: true }) videoRef!: ElementRef;
 
+  ngAfterViewInit(): void {
+    const media = this.videoRef.nativeElement;
+    media.muted = true;
+    media.play();
+  }
 }
